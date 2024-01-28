87d79e0b139ccc7bd60181da8a2318dc1c8c77be
Vedaant Nookala <nookala2@wisc.edu>
2024-01-27 18:08:01 -0600

    added shortcut for nabla functionality

---
+++ init.lua
@@ -0,0 +1,23 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+
+
+-- user defined function 
+vim.api.nvim_create_user_command('NewNote','call nvim_buf_set_lines(0, 0, 0, v:true, ["test text"])',{})
+vim.api.nvim_create_user_command('Cs300','call (nvim-tree-api.tree.reload)',{})
+-- color scheme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme tokyonight]])
+
+-- setup for vimwiki
+
+vim.cmd('set nocompatible')
+vim.cmd('filetype plugin on')
+vim.cmd('syntax on')
+
+
+
+-- user settings
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/core/plugins.lua
@@ -0,0 +1,54 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-orgmode/orgmode'
+  use 'lewis6991/gitsigns.nvim'
+  use 'nvim-lualine/lualine.nvim'
+   use({
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.4',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  })
+use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
+use{
+  'folke/tokyonight.nvim'
+}
+use {'neoclide/coc.nvim', branch = 'release'}
+use{'rebelot/kanagawa.nvim'}
+use {
+  'serenevoid/kiwi.nvim', 
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+use{
+  'CRAG666/code_runner.nvim'
+}
+use{'vimwiki/vimwiki'}
+use{'goolord/alpha-nvim',
+use { "ellisonleao/gruvbox.nvim" }
+}
+use{'itchyny/calendar.vim'}
+use{'lukas-reineke/indent-blankline.nvim'}
+use 'jbyuki/nabla.nvim'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
